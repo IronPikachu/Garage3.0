647f7ac1599253d4e266c37f9cfe0440a8a161e8
Cecilia Sjögren <cisci_13@hotmail.com>
2022-03-08 15:32:08 +0100

    .

--- Garage 2.0/Data/GarageVehicleContext.cs
+++ Garage 2.0/Data/GarageVehicleContext.cs
@@ -16,8 +16,8 @@ public class GarageVehicleContext : DbContext
         Global.Garagecapacity = config.GetValue<int>("GarageCapacity:Capacity");
     }
 
-    public DbSet<Garage_2._0.Models.Vehicle> Vehicle { get; set; }
-    public DbSet<Garage_2._0.Models.Member> Member { get; set; }
+    public DbSet<Vehicle> Vehicle { get; set; }
+    public DbSet<Member> Member { get; set; }
     public DbSet<VehicleType> VehicleType { get; set; }
     public DbSet<Membership> Membership { get; set; }
     public DbSet<MemberHasMembership> MemberHasMembership { get; set; }
@@ -127,42 +127,5 @@ public class GarageVehicleContext : DbContext
 
         return vehicleTypes;
     }
-    private string MakeSocialSecurityNumber()
-    {
-        DateTime start = new DateTime(1900, 01, 01);
-        int range = (DateTime.Now - start).Days;
-        string birthDate = (start.AddDays(gen.Next(range))).ToString();
-        string birthPlace = (gen.Next(14, 99)).ToString();
-        string gender = (gen.Next(1, 9)).ToString();
-        string firstNineNumbers = birthDate + birthPlace + gender;
-        string controlnumber = generateControlNumber(firstNineNumbers);
-        string SSN = firstNineNumbers + controlnumber;
-        if (Member.Find(SSN) == null)
-            return firstNineNumbers + controlnumber;
-        else
-            return MakeSocialSecurityNumber();
-    }
-    private string generateControlNumber(string nineDigits)
-    {
-        string newNumbers = "";
-        for (int i = 0; i < nineDigits.Length - 1; i++)
-        {
-            if (i % 2 == 0)
-                newNumbers += (int.Parse(nineDigits[i].ToString()) * 2).ToString();
-            else
-                newNumbers += nineDigits[i].ToString();
-        }
-        //Sets the last of the 4 digits (control number)
-        int controlNumber = 0;
-
-        //goes true all of the new numbers one by one and adds them together
-        foreach (char n in newNumbers)
-        {
-            controlNumber += int.Parse(n.ToString());
-        }
-
-        //The formula to calculate the correct control number
-        controlNumber = (10 - (controlNumber % 10)) % 10;
-        return controlNumber.ToString();
-    }
+    
 }
